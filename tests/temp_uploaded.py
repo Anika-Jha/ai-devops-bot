d6279c09389015d23619e2188dc1c3d735c52203
Anika Jha <113995788+Anika-Jha@users.noreply.github.com>
2025-06-17 15:16:01 +0530

    Delete tests/temp_uploaded.py

--- tests/temp_uploaded.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-URL = "https://hmdb.ca/metabolites?utf8=%E2%9C%93&query=glucose&search_field=all"
-HEADERS = {"User-Agent": "Mozilla/5.0"}
-
-response = requests.get(URL, headers=HEADERS)
-print("Status Code:", response.status_code)
-print("Response Headers:", response.headers)
-print("First 500 Characters of Response:", response.text[:500])
